ee97b27d2774419e07e5109c2e12eed445d77b19
Diego Mantilla <dmantill44@gmail.com>
2026-08-06 08:42:57 -0500

    refactor(app) 🚀: simplify Main entry point and update build script

--- compile.bat
+++ compile.bat
@@ -3,7 +3,7 @@ echo Compilando TaskFlow...
 
 if not exist "out" mkdir out
 
-javac -cp "lib/*" -d out src/main/java/com/taskflow/model/*.java src/main/java/com/taskflow/repository/*.java src/main/java/com/taskflow/service/*.java src/main/java/com/taskflow/ui/*.java src/main/java/com/taskflow/*.java
+javac -cp "lib/*" -d out src/main/java/com/taskflow/model/*.java src/main/java/com/taskflow/ui/*.java src/main/java/com/taskflow/*.java
 
 if %ERRORLEVEL% equ 0 (
     echo Compilacion exitosa.
